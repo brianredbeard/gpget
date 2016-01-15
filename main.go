3d71600516254e2e0ea70498bac55ac5fb42ad1b
Brian 'Redbeard' Harrington <redbeard@dead-city.org>
2016-01-15 11:48:30 -0800

    fix: Initially working
    Fixed a few type issues and ensured the basic mechanism works.

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"code.google.com/p/go.crypto/openpgp"
 	"flag"
 	"fmt"
-	//	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"strconv"
+	"path"
+	"strings"
 )
 
 /*
@@ -22,8 +24,8 @@ GPG/PGP versus just assuming it's on the local keyring, so be it.
 var (
 	flagHelp = flag.Bool("help", false, "print help and exit")
 	flagURL  = flag.String("url", "", "url to retrieve")
-	flagMem  = flag.Bool("mem", false, "keep the download buffer in memory")
-	flagPath = flag.String("path", "./", "location to place successful download")
+	flagMem  = flag.Bool("O", false, "save the file using it's original name")
+	flagPath = flag.String("o", "./", "location to place successful download")
 )
 
 /*
@@ -43,6 +45,11 @@ type File struct {
 	content   []byte
 }
 
+type SigState struct {
+	success bool
+	sig     string
+}
+
 func main() {
 
 	flag.Parse()
@@ -52,9 +59,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	thing := dlFile(*flagURL)
+	gpfile := dlFile(*flagURL)
 
-	fmt.Println(strconv.Itoa(len(thing.content)))
+	state, err := checkGPG(gpfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !state.success {
+		fmt.Println("Unknown error in retrieving your file")
+		os.Exit(3)
+	}
+
+	if *flagMem {
+		f, err := os.Create(gpfile.name)
+		if err != nil {
+			fmt.Printf("Error opening file %s for write - %s", gpfile.name, err)
+			os.Exit(5)
+		}
+
+		n, err := f.Write(gpfile.content)
+
+		if err != nil {
+			fmt.Printf("Unable to save file, %d bytes written of %d bytes total - %s\n", n, len(gpfile.content), err)
+			os.Exit(5)
+		}
+
+	} else {
+		fmt.Println(string(gpfile.content))
+	}
 	os.Exit(0)
 }
 
@@ -62,52 +94,86 @@ func main() {
 // and return the byte stream
 
 func getRemote(url string) (data []byte, err error) {
-	/* getRemote(
-	 */
 	resp, err := http.Get(url)
+
 	if err != nil {
 		fmt.Println("Could not download " + url)
 		return data, err
 	}
 
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Could not download the url %s\n", url)
+		os.Exit(2)
+	}
+
 	data, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
 
 	return data, err
 }
 
-// download a specified file and look for two well formatted signatures of the
-// form "url" + ".sig" & "url" + ".asc" representing the binary and base64 gpg
-// signatures respectively
+// download a specified file and look for a well formatted signature of the
+// form "url" + ".asc" representing the binary and base64 gpg signatures
+// respectively
 
 func dlFile(url string) (file File) {
 	var myfile File
 	var err error
 	var armor []byte
-	fmt.Println("Downloading File")
 	asc := url + ".asc"
-	sig := url + ".sig"
 
+	myfile.name = path.Base(url)
 	myfile.content, err = getRemote(url)
-	armor, err = getRemote(asc)
-	myfile.armor = string(armor)
-	myfile.signature, err = getRemote(sig)
 
 	if err != nil {
 		if err == http.ErrMissingFile {
-			fmt.Println("no armor file")
+			fmt.Printf("Error (404): the file %s did not exist\n", url)
+			os.Exit(1)
 		} else {
-			fmt.Println("Error retreiving armored signature: " + url + ".asc")
+			fmt.Printf("Transport error retreiving %s\n" + url)
 			os.Exit(1)
 		}
 	}
 
+	armor, err = getRemote(asc)
+
+	if err != nil {
+		if err == http.ErrMissingFile {
+			fmt.Printf("Error (404): the file %s did not exist, you must sign all files with an ASCII armored signature", url+".asc")
+			os.Exit(1)
+		} else {
+			fmt.Println("Transport error retreiving armored signature: " + url + ".asc")
+			os.Exit(1)
+		}
+	}
+
+	myfile.armor = string(armor)
+
 	return myfile
 }
 
-func checkGPG(file File) {
-	keys, _ := os.Open("~/.gnupg/pubring.gpg")
-	keyring, _ := openpgp.ReadKeyRing(keys)
+func checkGPG(file File) (state SigState, err error) {
+	keypath := path.Join(os.Getenv("HOME"), "/.gnupg/pubring.gpg")
+	keys, err := os.Open(keypath)
+	if err != nil {
+		fmt.Printf("Could not open public keyring at %s\n", keypath)
+		os.Exit(2)
+	}
 
-	fmt.Println("%T : %#v\n", keyring, keyring)
-	//signer, err := openpgp.CheckArmoredDetachedSignature(keyring, file.content, io.Reader(file.signature))
+	keyring, err := openpgp.ReadKeyRing(keys)
+
+	if err != nil {
+		fmt.Printf("Error reading public keyring: %s\n", err)
+		os.Exit(2)
+	}
+
+	signer, err := openpgp.CheckArmoredDetachedSignature(keyring, bytes.NewReader(file.content), strings.NewReader(file.armor))
+
+	if err != nil {
+		fmt.Printf("Invalid signature or public key not present: %s\n", err)
+		os.Exit(2)
+	}
+	state.sig = signer.PrimaryKey.KeyIdShortString()
+	state.success = true
+	return state, nil
 }
